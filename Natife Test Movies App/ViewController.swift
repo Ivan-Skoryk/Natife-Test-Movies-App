9701dbaacc7541bbb0430fd0e71c45a4277cf77b
Ivan Skoryk <ivan.skoryk@icloud.com>
2023-10-07 01:20:41 +0300

    Initial Commit

---
+++ Natife Test Movies App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Natife Test Movies App
+//
+//  Created by Ivan Skoryk on 07.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
